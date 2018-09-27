3ba50edc6d24ec591dae05012fbca3ee22bb7642
ColorizeMySky <firiziant@gmail.com>
2018-09-28 01:36:43 +0300

    Fix bug with glass's shadow, add indicator of current color

--- pixel-art.js
+++ pixel-art.js
@@ -1,5 +1,6 @@
 let canvas = document.querySelector('.canvas');
 let colorHolders = document.querySelectorAll('.color_holder');
+let currentIndicators = document.querySelectorAll('.current_color');
 
 //because in CSS the color made by using gradient and not the same of string color name
 let colorsReplace = {
@@ -20,15 +21,19 @@ for (color of colorHolders) {
 	color.addEventListener('click', function(){
 		let getColor = this.classList[color.classList.length - 1];	
 		getColor =  getColor.replace(/(\w+)-\w+/, '$1');
-		currentColor = 	colorsReplace[getColor];		
+		currentColor = 	colorsReplace[getColor];
+
+		//change color of indicators
+		for (indicator of currentIndicators) {
+			indicator.style.backgroundColor = currentColor;
+		}	
 		return currentColor;		
 	})	
 }
 
-//a listener for colorize table-data by click with using bubble
+//a listener for colorize dot by click with using bubble
 canvas.addEventListener('click', function(elem) {
 	if(elem.target.classList.contains("canvas-dot")) {
-		console.log(currentColor);
 		elem.target.style.backgroundColor = currentColor;
 	}
 });
